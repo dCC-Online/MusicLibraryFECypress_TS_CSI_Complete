d5ab30ef94665c1d489425b1309285e87538b877
Cash Myers <cmyers880@gmail.com>
2022-10-06 09:23:38 -0500

    fix: Added <thead> and <tbody> tags to MusicTable

--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -25,7 +25,7 @@ const MusicTable = (props) => {
     console.log("Editing song", song);
     setSong(song);
     showModal();
-   }
+  }
 
   const deleteSong = async (key) => {
     await axios.delete(`http://127.0.0.1:8000/music/${key}/`)
@@ -36,30 +36,34 @@ const MusicTable = (props) => {
     return (
       <div>
         <table className="center">
-          <tr>
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Genre</th>
-            <th>Release Date</th>
-          </tr>
-          {props.songs.map((song) => {
-            return (
-              <tr key={song.id}>
-                <td data-test="song-title">{song.title}</td>
-                <td>{song.artist}</td>
-                <td>{song.album}</td>
-                <td>{song.genre}</td>
-                <td>{song.release_date}</td>
-                <td><button onClick={() => deleteSong(song.id)} type="submit">
-                  Delete Song
-                </button></td>
-                <td><button type="button" onClick={() => handleEdit(song)}>
-                  Edit Song
-                </button></td>
-              </tr>
-            );
-          })}
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Genre</th>
+              <th>Release Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.songs.map((song) => {
+              return (
+                <tr key={song.id}>
+                  <td data-test="song-title">{song.title}</td>
+                  <td>{song.artist}</td>
+                  <td>{song.album}</td>
+                  <td>{song.genre}</td>
+                  <td>{song.release_date}</td>
+                  <td><button onClick={() => deleteSong(song.id)} type="submit">
+                    Delete Song
+                  </button></td>
+                  <td><button type="button" onClick={() => handleEdit(song)}>
+                    Edit Song
+                  </button></td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
         {/* Proper location for modal display */}
         <EditSong show={show} handleClose={hideModal} props={song}>
